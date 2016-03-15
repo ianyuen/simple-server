6a8783e64dffcf948612c20805b579a97e06595c
ianyuen <thanhianyuen@gmail.com>
2016-03-15 21:55:00 +0700

    - try use parameter for request

--- app/index.js
+++ app/index.js
@@ -9,6 +9,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
 	res.send('<html><body><h1>Hello World</h1></body></html>');
 });
+app.get('/:a?/:b?/:c?', function (req,res) {
+	res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+});
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
