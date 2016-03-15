08cd169b88e0a0da4de0818ead59e2b38fda5b48
ianyuen <thanhianyuen@gmail.com>
2016-03-15 22:24:21 +0700

    - use jade to implement 404 page

--- app/index.js
+++ app/index.js
@@ -4,7 +4,13 @@ var path = require('path');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req,res) {
+	res.render('404', {url:req.url});
+});
 
 app.get('/', function (req, res) {
 	res.send('<html><body><h1>Hello World</h1></body></html>');
