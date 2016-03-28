52d65c9a583ccfd053418b906c819aa9af81e99d
ianyuen <thanhianyuen@gmail.com>
2016-03-28 21:21:35 +0700

    + log.js (for debug)

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
 var http = require('http');
-var express = require('express');
 var path = require('path');
+var express = require('express');
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -23,37 +23,5 @@ http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/local');
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-	console.log("we're connected!")
-});
-
-var kittySchema = mongoose.Schema({
-	name: String
-});
-var Kitten = mongoose.model('Kitten', kittySchema);
-var silence = new Kitten({ name: 'Silence' });
-console.log(silence.name);
-
-kittySchema.methods.speak = function () {
-	var greeting = this.name ? "Meow name is " + this.name : "I don't have a name";
-	console.log(greeting);
-}
-
-var Kitten = mongoose.model('Kitten', kittySchema);
-
-var fluffy = new Kitten({ name: 'fluffy' });
-fluffy.save(function (err, fluffy) {
-	if (err)
-		return console.error(err);
-});
-
-Kitten.find(function (err, kittens) {
-	if (err)
-		return console.error(err);
-	console.log(kittens);
-})
+var log = require('./log.js');
+log.print("test");
