d8db0a89e112360426e17a4db8fe802570aaddd0
ianyuen <thanhianyuen@gmail.com>
2016-04-12 22:11:47 +0700

    - insert user function

--- app/database.js
+++ app/database.js
@@ -1,46 +1,25 @@
 var url = 'mongodb://localhost:27017/WorkoutSolution';
 var assert = require('assert');
-var MongoClient = require('mongodb').MongoClient;
+var mongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect(url, function(err, db) {
+mongoClient.connect(url, function(err, db) {
 	assert.equal(null, err);
-	log.print("Connected correctly to server.");
 	db.close();
 });
 
-var insertDocument = function(db, callback) {
-	db.collection('restaurants').insertOne( {
-		"address" : {
-			"street" : "2 Avenue",
-			"zipcode" : "10075",
-			"building" : "1480",
-			"coord" : [ -73.9557413, 40.7720266 ]
-		},
-		"borough" : "Manhattan",
-		"cuisine" : "Italian",
-		"grades" : [
-			{
-				"date" : new Date("2014-10-01T00:00:00Z"),
-				"grade" : "A",
-				"score" : 11
-			},
-			{
-				"date" : new Date("2014-01-16T00:00:00Z"),
-				"grade" : "B",
-				"score" : 17
-			}
-		],
-		"name" : "Vella",
-		"restaurant_id" : "41704620"
+var insertDocument = function(db, username, password, callback) {
+	db.collection('users').insertOne( {
+		"username" : username,
+		"password" : password
 	}, function(err, result) {
 		assert.equal(err, null);
-		log.print("Inserted a document into the restaurants collection.");
 		callback();
+		console.log("Inserted " + username + " to database");
 	});
 };
 
 var findRestaurants = function(db, callback) {
-	var cursor =db.collection('restaurants').find( { "borough": "Manhattan" } );
+	var cursor = db.collection('restaurants').find( { "borough": "Manhattan" } );
 	cursor.each(function(err, doc) {
 		assert.equal(err, null);
 		if (doc != null) {
@@ -49,4 +28,15 @@ var findRestaurants = function(db, callback) {
 			callback();
 		}
 	});
+};
+
+module.exports = {
+	insertUser: function (username, password) {
+		mongoClient.connect(url, function(err, db) {
+			assert.equal(null, err);
+			insertDocument(db, username, password, function() {
+				db.close();
+			});
+		});
+	}
 };
--- app/index.js
+++ app/index.js
@@ -1,8 +1,15 @@
+var log = require('./log.js');
 var http = require('http');
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser')
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -11,15 +18,21 @@ app.get('/', function(req, res) {
 	res.send('<html><body><h1>Hello World</h1></body></html>');
 });
 app.get('/:a?/:b?/:c?', function(req, res) {
-	res.send(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+	res.send(req.body.params.a + ' ' + req.params.b + ' ' + req.params.c);
 });
 
-app.post('/:a?/:b?/:c?', function(req, res) {
-	log.print(req.params.a + ' ' + req.params.b + ' ' + req.params.c);
+app.post('/', function(req, res) {
+	var action = req.body.action;
+	if (action == "signup") {
+		signup(req.body.username, req.body.password);
+	}
 });
+
+function signup(username, password) {
+	var database = require('./database.js')
+	database.insertUser(username, password);
+}
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
 });
-
-var log = require('./log.js');
